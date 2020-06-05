2717840e3862c43862b19dc58133099ff79eb604
Chunguang Wang <chunguangwang@Chunguangs-MacBook-Pro.local>
2020-06-04 21:50:45 -0700

    modifying buildspec.yml

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'http://localhost:5000';
+  const ENDPOINT = 'http://www.chunguangpenisula.net:5000';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
